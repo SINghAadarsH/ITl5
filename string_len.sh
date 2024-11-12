1d70bf7a253672af46118b30d876648d6c6dc508
SINghAadarsH <adarshsinghsreeneth001@gmail.com>
2024-11-12 09:28:15 +0530

    feature 1 added successfully

--- string_len.sh
+++ string_len.sh
@@ -6,4 +6,14 @@ myVarLength=${#myVar}
 
 echo "length is $myVarLength"
 
+#added 2 more feature which converts our string to uppercase and lowercase
+
+# Convert to uppercase and display the string (not the length)
+upperCase=${myVar^^}
+echo "upper case $upperCase"
+
+# Convert to lowercase and display the string (not the length)
+lowerCase=${myVar,,}
+echo "lower case $lowerCase"
+
 
